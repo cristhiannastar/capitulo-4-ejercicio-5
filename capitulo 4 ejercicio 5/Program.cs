d2d77efefa092aa505a23a8b6521574dfd3e8cad
Hp <Hp@DESKTOP-NVV8KB0>
2022-11-16 21:21:01 -0500

    Agregar archivos de proyecto.

---
+++ capitulo 4 ejercicio 5/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ejercicio4
+{
+    public class program
+    {
+        public static void Main()
+        {
+            int edad = 1, suma = 0, c = 0;
+            float promedio;
+            while (edad != 0)
+            {
+                Console.WriteLine("ingrese una edad: ");
+                edad = Convert.ToInt32(Console.ReadLine());
+                if (edad > 0)
+                {
+                    suma = suma + edad;
+                    c++;
+                }
+            }
+
+            promedio = suma / c;
+            Console.WriteLine("el promedio es:" + promedio);
+        }
+    }
+}
+
+
+
